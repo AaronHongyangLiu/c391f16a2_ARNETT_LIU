f23a2d1528caac658d26b7578ad7c9a60b2887cb
Aaron Liu <supermanaaronliu@gmail.com>
2016-10-05 19:41:41 -0700

    q0 and q1

--- q0.py
+++ q0.py
@@ -13,14 +13,15 @@ def main(filename):
 
     latPerTenMeter = 10.0/111191
     lonPerTenMeter = 10.0/74539
-    height = (latPerTenMeter/deltaLat) * 1000
-    width = (lonPerTenMeter/deltaLon) * 1000
+    height = (latPerTenMeter/deltaLat) * 1000  # height and width for a 10mX10m box on the grid
+    width = (lonPerTenMeter/deltaLon) * 1000   #
 
-    infile = open(filename, 'r')
+    infile = open(filename, 'r')  # read the data file
     lines = infile.readlines()
     infile.close()
 
-    outfile = open('newData.tsv', 'w')
+    outfile1 = open('newData.tsv', 'w') # new data for poi table
+    outfile2 = open('rtreeData.tsv', 'w') # new data for rtree table
     for line in lines:
 	line = line.strip()
         items = line.split('\t')
@@ -30,9 +31,11 @@ def main(filename):
         maxX = newLon + width
         minY = newLat - height
         maxY = newLat
-        outfile.write("%s\t%s\t%s\t%s\t%f\t%f\t%f\t%f\t%f\t%f\n" % (items[0], items[1], items[2], items[3], newLat, newLon, minX, maxX, minY, maxY))
+        outfile1.write("%s\t%s\t%s\t%s\t%f\t%f\t%f\t%f\t%f\t%f\n" % (items[0], items[1], items[2], items[3], newLat, newLon, minX, maxX, minY, maxY))
+        outfile2.write("%s\t%f\t%f\t%f\t%f\n" % (items[0], minX, maxX, minY, maxY))
 
-    outfile.close()
+    outfile1.close()
+    outfile2.close()
 
 if len(sys.argv) == 2:
     main(sys.argv[1])
