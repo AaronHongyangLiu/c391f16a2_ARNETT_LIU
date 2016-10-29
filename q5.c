3f7b94588f2dce53be23956045b510faaa6bb6c3
tdarnett <tarnett1@mac.com>
2016-10-29 15:10:41 -0700

    q5 now uses an array and benchmarks are timed without sqlite_prepare

--- q5.c
+++ q5.c
@@ -1,4 +1,5 @@
 #define _GNU_SOURCE // for asprintf
+
 #include <stdio.h>
 #include <sqlite3.h>
 #include <string.h>
@@ -13,7 +14,8 @@ double time_query(sqlite3 **db_ptr, double min_Xi, double max_Xi, double min_Yi,
 
 int main(int argc, char **argv) {
     sqlite3 *db; //the database
-    srand(time(NULL)); //for generating random numbers (from http://stackoverflow.com/questions/822323/how-to-generate-a-random-number-in-c)
+    srand(time(
+            NULL)); //for generating random numbers (from http://stackoverflow.com/questions/822323/how-to-generate-a-random-number-in-c)
 
     int rc;
 
@@ -38,31 +40,39 @@ int main(int argc, char **argv) {
     }
 
     float total_std = 0, total_rtree = 0;
-    for (int i = 0; i < QUERY_RUNS; i++) {
+
+    // MAX_BOUNDING_SQUARES * 4 because there are 4 coordinates per square
+    double bounding_squares_array[
+            MAX_BOUNDING_SQUARES * 4]; // [min_X1, max_X1, min_Y1, max_Y1, min_X2, max_X2, min_Y2, max_Y2,...]
+
+    for (int i = 0; i < MAX_BOUNDING_SQUARES * 4; i += 4) {
         // randomly generates the bottom left coordinate of the new box
         double x = rand() % (1000 - side_length_int + 1); // random integer between 0 - 1000 specifying X coord
         double y = rand() % (1000 - side_length_int + 1); // random integer between 0 - 1000 specifying Y coord
 
-        double minX = x;
-        double maxX = x + side_length;
-        double minY = y;
-        double maxY = y + side_length;
+        bounding_squares_array[i] = x;
+        bounding_squares_array[i + 1] = x + side_length;
+        bounding_squares_array[i + 2] = y;
+        bounding_squares_array[i + 3] = y + side_length;
 
-        double total_time_std = 0, total_time_rtree = 0;
-        for (int j = 0; j < MAX_BOUNDING_SQUARES; j++) {
-            total_time_std += time_query(&db, minX, maxX, minY, maxY, 0);
-            total_time_rtree += time_query(&db, minX, maxX, minY, maxY, 1);
-        }
-        total_std += (total_time_std/1000/QUERY_RUNS);
-        total_rtree += (total_time_rtree/1000/QUERY_RUNS);
-
-//        printf("Parameter l: %d \n"
-//               "Average runtime with r-tree: %lf ms\n"
-//               "Average runtime without r-tree: %lf ms\n", side_length, total_time_rtree/1000/QUERY_RUNS, total_time_std/1000/QUERY_RUNS);
     }
+    double total_time_std = 0, total_time_rtree = 0;
+    for (int i = 0; i < QUERY_RUNS; i++) {
+        for (int j = 0; j < MAX_BOUNDING_SQUARES * 4; j += 4) {
+            total_time_std += time_query(&db, bounding_squares_array[j], bounding_squares_array[j + 1],
+                                         bounding_squares_array[j + 2], bounding_squares_array[j + 3], 0);
+            total_time_rtree += time_query(&db, bounding_squares_array[j], bounding_squares_array[j + 1],
+                                           bounding_squares_array[j + 2], bounding_squares_array[j + 3], 1);
+        }
+    }
+    total_std += (total_time_std / 1000 / QUERY_RUNS);
+    total_rtree += (total_time_rtree / 1000 / QUERY_RUNS);
+
+
     printf("Parameter l: %.4f \n"
-           "Average runtime with r-tree: %lf ms\n"
-           "Average runtime without r-tree: %lf ms\n", side_length, total_rtree/MAX_BOUNDING_SQUARES, total_std/MAX_BOUNDING_SQUARES);
+                   "Average runtime with r-tree: %lf ms\n"
+                   "Average runtime without r-tree: %lf ms\n", side_length, total_rtree / MAX_BOUNDING_SQUARES,
+           total_std / MAX_BOUNDING_SQUARES);
 
     sqlite3_close(db);
 }
@@ -80,7 +90,6 @@ double time_query(sqlite3 **db_ptr, double min_Xi, double max_Xi, double min_Yi,
      * param Rtree: a boolean specifying the table to run the query on
      */
     double time = 0;
-    int rc;
     sqlite3_stmt *stmt; //the update statement
     char *min_X, *max_X, *min_Y, *max_Y;
 
@@ -107,7 +116,7 @@ double time_query(sqlite3 **db_ptr, double min_Xi, double max_Xi, double min_Yi,
 
     // initialize the statement of appropriate length
     char sql_stmt[strlen(stmt_1) + strlen(stmt_2) + strlen(stmt_3) + \
-                  strlen(stmt_4) + strlen(stmt_5) + strlen(min_X) +  \
+                  strlen(stmt_4) + strlen(stmt_5) + strlen(min_X) + \
                   strlen(min_Y) + strlen(max_X) + strlen(max_Y)];
 
     strcpy(sql_stmt, stmt_1);
@@ -122,13 +131,7 @@ double time_query(sqlite3 **db_ptr, double min_Xi, double max_Xi, double min_Yi,
     clock_t before, after, diff;
 
     before = clock();// start timer
-    // test if the query can be prepared
-    rc = sqlite3_prepare_v2(*db_ptr, sql_stmt, -1, &stmt, 0);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Preparation failed: %s\n", sqlite3_errmsg(*db_ptr));
-        return 1;
-    }
-    sqlite3_exec(*db_ptr,sql_stmt,NULL,NULL,NULL);
+    sqlite3_exec(*db_ptr, sql_stmt, NULL, NULL, NULL);
     after = clock(); // stop timer
 
     diff = (after - before);
